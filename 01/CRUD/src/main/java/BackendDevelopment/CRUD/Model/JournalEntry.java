14068386ed17d9599ca226644b471aac36abe5cc
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-03-14 15:22:36 -0300

    Classe JournalEntry.java

--- 01/CRUD/src/main/java/BackendDevelopment/CRUD/Model/JournalEntry.java
+++ 01/CRUD/src/main/java/BackendDevelopment/CRUD/Model/JournalEntry.java
@@ -1,5 +1,37 @@
+@Entity
 public class JournalEntry{
 
+    @id
     private int id;
     private String title;
+    private String category;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+       
+    public String getTitle() {
+        return title;
+    }
+       
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    } 
+    @Override
+    public String toString() {
+        return "JournalEntry [id=" + id + ", title=" + title + ", category=" + category + "]";
+    }
+    
 }
