7731fe17488222c5c4de2a0dd4d2e8cc918cd6b9
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-03-20 11:09:10 -0300

    Atualizando repository JournalEntryRepository.java

--- 01/CRUD/src/main/java/com/example/CRUD/Repository/JournalEntryRepository.java
+++ 01/CRUD/src/main/java/com/example/CRUD/Repository/JournalEntryRepository.java
@@ -1,8 +1,16 @@
 package com.example.CRUD.Repository;
 
 import com.example.CRUD.Model.JournalEntry;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JournalEntryRepository extends JpaRepository<JournalEntry, Integer>{
-    
+    List<JournalEntry>findByCategory(String category);
+    List<JournalEntry>findByIdGreaterThan(int id);
+
+    @Query("from JournalEntry where category=?1 order by title")
+    List<JournalEntry>findByCategorySorted(String category);
 }
