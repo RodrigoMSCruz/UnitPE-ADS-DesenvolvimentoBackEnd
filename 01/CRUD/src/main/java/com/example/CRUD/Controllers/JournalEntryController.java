a1071fa4567cba95ea37a2e9ccf5b75122baf6b2
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-03-23 11:28:26 -0300

    Atualizando JournalEntryController.java

--- 01/CRUD/src/main/java/com/example/CRUD/Controllers/JournalEntryController.java
+++ 01/CRUD/src/main/java/com/example/CRUD/Controllers/JournalEntryController.java
@@ -1,5 +1,28 @@
 package com.example.CRUD.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.CRUD.Models.JournalEntry;
+import com.example.CRUD.Repositories.JournalEntryRepository;
+
+@Controller
 public class JournalEntryController {
+    @Autowired
+    JournalEntryRepository repo;
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/addEntry")
+    public String addJournalEntry(JournalEntry journalEntry){
+        repo.save(journalEntry);
+        return "home";
+    }
+
+
     
 }
