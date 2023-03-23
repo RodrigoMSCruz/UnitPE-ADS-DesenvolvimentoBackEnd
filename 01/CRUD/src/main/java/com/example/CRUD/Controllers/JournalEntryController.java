35d0de0d8235017dbcb6021e80d3e8ceab28256a
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-03-23 16:02:03 -0300

    Adicionando mais métodos no JournalEntryController.java direto do modelo do professor.

--- 01/CRUD/src/main/java/com/example/CRUD/Controllers/JournalEntryController.java
+++ 01/CRUD/src/main/java/com/example/CRUD/Controllers/JournalEntryController.java
@@ -2,6 +2,11 @@ package com.example.CRUD.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.CRUD.Models.JournalEntry;
@@ -23,6 +28,59 @@ public class JournalEntryController {
         return "home";
     }
 
+    @RequestMapping("/getAllEntries")
+	public ModelAndView getAllJournalEntries() {
+		ModelAndView mv = new ModelAndView();
+		List<JournalEntry> entries = repo.findAll();
+		mv.addObject("entries", entries);
+		mv.setViewName("getAllEntries");
+		return mv;
+	}
 
+    @RequestMapping("/getEntry")
+	public ModelAndView getJournalEntry(@RequestParam int id) {
+		ModelAndView mv = new ModelAndView();
+		JournalEntry entry = repo.findById(id).orElse(new JournalEntry());
+		mv.addObject("entry", entry);
+		mv.setViewName("getEntry");
+		return mv;
+	}
+
+    @RequestMapping("/deleteEntry")
+	public String deleteJournalEntry(@RequestParam int id) {
+		repo.deleteById(id);
+		return "home";
+	}
+
+
+    @RequestMapping("/getEntriesByCategory")
+    public ModelAndView getEntriesByCategory(@RequestParam String category){
+        ModelAndView mv = new ModelAndView();
+		List<JournalEntry> entries = repo.findByCategory(category);
+		mv.addObject("entries", entries);
+		mv.setViewName("getEntriesByCategory");
+		return mv;
+    }
+
+    @RequestMapping("/getEntriesByIdGT")
+	public ModelAndView getEntriesByIdGT(@RequestParam int id) {
+		ModelAndView mv = new ModelAndView();
+		List<JournalEntry> entries = repo.findByIdGreaterThan(id);
+		mv.addObject("entries", entries);
+		mv.setViewName("getEntriesByIdGT");
+		return mv;
+	}
+
+	// Find By Category but sorted by title
+	@RequestMapping("/getEntriesByCategorySorted")
+	public ModelAndView getEntriesByCategorySorted(@RequestParam String category) {
+
+		ModelAndView mv = new ModelAndView();
+		List<JournalEntry> entries = repo.findByCategorySorted(category);
+		System.out.println(repo.findByCategorySorted(category));
+		mv.addObject("entries", entries);
+		mv.setViewName("getEntriesByCategorySorted");
+		return mv;
+	}
     
 }
